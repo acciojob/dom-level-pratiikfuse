6f72faca64222dd2672fbdbc958236d421761ec1
Pratik Fuse <pratikfuse20@gmail.com>
2023-09-27 11:41:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function getDOMLevel()
+{
+	const levelElement = document.getElementById('level');
+	let count  = 0;
+	while(levelElement.parentNode)
+		{
+			levelElement = levelElement.parentNode;
+			count++;
+		}
+	alert("The level of the element is: "+count);
+}
+
+getDOMLevel();
